398630a50554aa11edb678ea00ab068348468f91
Your Name <you@example.com>
2016-11-06 18:36:17 +0000

    A number of hosts greater than 128 but less than 254 should now work as expected.

--- lib/subnet_calc.rb
+++ lib/subnet_calc.rb
@@ -30,22 +30,39 @@ class SubnetCalc
     default_inputs = {hosts: 254, ip: '192.168.0.0'}.merge(inputs)
 
     @prefixes = 1.upto(32).each_slice(8).to_a
+    
 =begin
     #=> [
          [1, 2, 3, 4, 5, 6, 7, 8], [9, 10, 11, 12, 13, 14, 15, 16], 
          [17, 18, 19, 20, 21, 22, 23, 24], [25, 26, 27, 28, 29, 30, 31, 32]
         ]
 =end
+    
 
-    octets = @prefixes.map do |octet|
+    octets = @prefixes.map.with_index do |octet, j|
 
       a = octet.map.with_index do |prefix, i|
-        OpenStruct.new prefix: prefix, decimal: 2 ** (octet.length - 1 - i )
+        
+        decimal = 2 ** (octet.length - 1 - i )
+
+        h = {
+              prefix: prefix,
+              decimal: decimal,
+              hosts_per_subnet: (2 ** 8) ** (@prefixes.length - 1 - j) * decimal
+            }
+
+        OpenStruct.new h
+        
       end
 
       OpenStruct.new mask: nil, bits: a
 
     end
+    
+    hosts_per_subnet = octets.map.with_index do |x,i| 
+      x.bits.map.with_index {|y,j| [i, j, y.hosts_per_subnet] }
+    end.flatten(1)
+    
 
     # determine what class of network we are using
 
@@ -74,19 +91,24 @@ class SubnetCalc
 
     hosts = default_inputs[:hosts]
 
+
+
     # find the smallest decimal value to match the 
-    # required number of hosts on a network
+    # required number of hosts on a network    
+    
+    octet_n, col = hosts_per_subnet.reverse.detect {|x| x.last >  hosts}    
 
-    col = octets[class_n].bits.reverse.detect {|x| x.decimal >  hosts }
+    bit = octets[octet_n].bits[col]
 
-    magic_number, prefix = col.decimal, col.prefix
-    n = octets[class_n].bits.map(&:decimal).index(magic_number)
-    no_of_subnets = 2 ** (n+1)
+    magic_number, prefix = bit.hosts_per_subnet, bit.prefix
+    no_of_subnets = (2 ** 8) / bit.decimal
 
+    
+    n =  col
 
     # add the new mask to the octet
 
-    octets[class_n].mask = octets[class_n].bits.map(&:decimal)[0..n].inject(:+)
+    octets[octet_n].mask = octets[octet_n].bits.map(&:decimal)[0..n].inject(:+)
     subnet_mask = octets.map(&:mask).join('.')
 
     subnets = no_of_subnets.times.inject([]) do |r,n|
@@ -119,7 +141,7 @@ class SubnetCalc
       max_subnets: no_of_subnets
     }
 
-    @class_n = class_n
+    @octet_n = octet_n
     @h = result
   end
 
@@ -137,11 +159,10 @@ class SubnetCalc
 
     tfo2 = TableFormatter.new
 
-    prefixes = @prefixes[@class_n].map {|x| '/' + x.to_s}
-    tfo2.source = [@h[:subnet_bitmask][@class_n].chars, prefixes]
+    prefixes = @prefixes[@octet_n].map {|x| '/' + x.to_s}
+    tfo2.source = [@h[:subnet_bitmask][@octet_n].chars, prefixes]
     tfo2.labels = 8.times.map {|n| (2 ** n).to_s }.reverse
     octet_table = tfo2.display(markdown: true).to_s
-
 
 <<EOF
 
@@ -172,7 +193,7 @@ Summary
 Breakdown
 ---------
 
-#{(@class_n + 1).ordinal} octet:
+#{(@octet_n + 1).ordinal} octet:
 
 #{indent octet_table}
 
@@ -191,7 +212,7 @@ EOF
     @h
   end
   
-  private
+  private  
   
   def indent(s, i=2)
     s.lines.map{|x| ' ' * i + x}.join
